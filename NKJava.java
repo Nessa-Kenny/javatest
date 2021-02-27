78a2b41572ef6bd78c154ccb7849bde880c07028
Nessa Kenny <x20144776@student.ncirl.ie>
2021-02-27 10:05:08 +0000

    1st upload of java

---
+++ NKJava.java
@@ -0,0 +1,13 @@
+package NessaSimpleJava;
+
+public class NKJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my simple Java Program");
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("The sum is:"+c);
+	}
+}
